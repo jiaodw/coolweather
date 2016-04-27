1b9f40065574fbfb6ffae2c02216193e96b7bb59
Lenovo <Lenovo>
2016-04-27 13:48:37 +0800

    add the database help class and the class to all table

---
+++ app/src/main/java/com/app/coolweather/model/County.java
@@ -0,0 +1,43 @@
+package com.app.coolweather.model;
+
+/**
+ * Created by Pradeep on 27/4/2016.
+ */
+public class County {
+    private int id;
+    private String countyName;
+    private String countyCode;
+    private int cityId;
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getCountyCode() {
+        return countyCode;
+    }
+
+    public void setCountyCode(String countyCode) {
+        this.countyCode = countyCode;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
---
+++ app/src/main/java/com/app/coolweather/model/Province.java
@@ -0,0 +1,34 @@
+package com.app.coolweather.model;
+
+/**
+ * Created by Pradeep on 27/4/2016.
+ */
+public class Province {
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+}
